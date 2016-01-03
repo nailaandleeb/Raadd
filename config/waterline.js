bf1e1ea1c917e5444a3e92ad1cfe76528129806e
nailaandleeb <naila.andleeb2013@gmail.com>
2016-01-04 01:41:22 +0500

    abc1

--- config/waterline.js
+++ config/waterline.js
@@ -17,11 +17,11 @@ module.exports = {
     mysqlDB: {
       adapter: 'mysqlAdapt',
       
-	 user : "sql6101744",
-		password :"FeEyYDyriK",
-		database : 'sql6101744',
-		host:"sql6.freemysqlhosting.net",
-		port:3306,
+	 user : process.env.OPENSHIFT_MYSQL_DB_USERNAME || "root",
+		password : process.env.OPENSHIFT_MYSQL_DB_PASSWORD || "",
+		database : process.env.OPENSHIFT_GEAR_NAME || 'raadd',
+		host:'163.180.116.192',
+		port:process.env.OPENSHIFT_MYSQL_DB_PORT,
 		
       supportBigNumbers:true, //true/false
       debug:false,  //['ComQueryPacket'], //false or array of node-mysql debug options
